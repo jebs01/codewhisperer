ff4dc975aae5bc52b645c20350d47d4cc566835f
Jeby <jebs01@gmail.com>
2023-02-09 19:29:59 +1100

    Create S3.py
    Intial commit

---
+++ S3.py
@@ -0,0 +1,15 @@
+# write a function to create S3 bucket to use as a public website
+import boto3
+s3 = boto3.resource('s3')
+def create_bucket(bucket_name, region=None):
+    try:
+        if region is None:
+            s3_client = boto3.client('s3')
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client = boto3.client('s3', region_name=region)
+            location = {'LocationConstraint': region}
+            s3_client.create_bucket(Bucket=bucket_name,
+                                    CreateBucketConfiguration=location)
+    except Exception as e:
+        print(e)
